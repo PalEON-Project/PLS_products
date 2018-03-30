8323a83f43ecbd64c83769e46ea38e286b7c2c97
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2018-03-29 18:08:14 -0700

    move get_angle for UMW to auxil.R

--- R/code/auxil.R
+++ R/code/auxil.R
@@ -44,3 +44,67 @@ get_angle_inil <- function(bearings, degrees, dists) {
   
 }
 
+get_angle_umw <- function(azimuth) {
+  #  This function is used in 'step.one.clean.bind_v1.1.R', it converts the
+  #  text azimuth strings to numeric, 360 degree values.
+  #  This is the vector that will store the values.
+  angle <- rep(NA, length(azimuth))
+  
+  #  This is a special case, generally where the tree is plot center.
+  angle[azimuth == '0'] <- 0
+  
+  #  This is a short function that takes cares of NAs in boolean functions, it's
+  #  just a simple wrapper for the boolean function that sets the NA values in
+  #  the vector to FALSE.
+  fx.na <- function(x) { x[ is.na( x ) ] <- FALSE; x }
+  
+  #  Given the text azimuths in the dataset, return the quadrant values.
+  #  This gives a boolean index of the quadrant
+  
+  north <- fx.na( regexpr('N', azimuth) > 0 )
+  east  <- fx.na( regexpr('E', azimuth) > 0 | azimuth == 'EAST')
+  south <- fx.na( regexpr('S', azimuth) > 0 | azimuth == 'SOUT')
+  west <-  fx.na( regexpr('W', azimuth) > 0 | azimuth == 'WEST')
+  
+  ne <- fx.na( (north & east) | azimuth == 'N  E')
+  se <- fx.na( (south & east) | azimuth == 'S  E')
+  sw <- fx.na( (south & west) | azimuth == 'S  W')
+  nw <- fx.na( (north & west) | azimuth == 'N  W') 
+  
+  #  The cell is in a quadrant, regardless of which.
+  quad <- ne | se | sw | nw
+  
+  #  Special case of the trees with a unidirectional direction.
+  uni  <- (!quad) & (north | south | east | west) & (nchar(azimuth) == 1)
+  
+  angle[ uni & north ] <- 360
+  angle[ uni & south ] <- 180
+  angle[ uni & east  ] <- 90 
+  angle[ uni & west  ] <- 270
+  
+  #  The problem is that some notes have either N04E, N 4E or N4E, or NE!
+  strlen <- nchar(azimuth)
+  strlen[is.na(azimuth)] <- NA
+  
+  angle[quad & strlen == 2] <- 45
+  angle[quad & strlen == 3] <- as.numeric(substr(azimuth[ quad & strlen == 3 ], 2, 2))
+  angle[quad & strlen == 4 & !substr(azimuth, 2, 3) == '  '] <- 
+    as.numeric(substr(azimuth[quad & strlen == 4 & !substr(azimuth, 2, 3) == '  '], 2, 3))
+  
+  # Special case of double spaces in the azimuth.
+  angle[quad & strlen == 4 & substr(azimuth, 2, 3) == '  '] <- 45
+
+    ## CJP: why is this not handled by code such as "azimuth=='EAST'" above? check on this
+  #  Another set of special cases:
+  angle[ fx.na(azimuth == 'NORT') ] <- 360
+  angle[ fx.na(azimuth == 'EAST') ] <- 90
+  angle[ fx.na(azimuth == 'WEST') ] <- 270
+  angle[ fx.na(azimuth == 'SOUT') ] <- 180
+  
+  angle[ se ] <- 180 - angle[ se ]
+  angle[ sw ] <- 180 + angle[ sw ]
+  angle[ nw ] <- 360 - angle [ nw ]
+  
+  return(angle)
+  
+}
