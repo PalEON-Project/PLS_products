6a4d14f63ce6a1907e501782407d70799714524d
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2018-03-29 18:00:04 -0700

    updated get_angle for IN/IL in light of KAH fixes to master

--- R/code/auxil.R
+++ R/code/auxil.R
@@ -4,75 +4,43 @@ convert_to_NA <- function(x, missingCodes = c(88888, 99999)) {
 }
 
 get_angle_inil <- function(bearings, degrees, dists) {
-  ##  This function converts the
-  ##  text azimuth strings to numeric, 360 degree values.
-  ##  This is the vector that will store the values.
-  angle <- degrees
-  
-  ##  This is a special case, generally where the tree is plot center.
-    ## CJP: this doesn't make any sense - angle is equal to degrees, so
-    ## if degrees is 0 then angle already is 0
-  if(FALSE) {
-      angle[degrees == 0 & dists == 0] <- 0
-  }
+    ##  This function converts the
+    ##  text azimuth strings to numeric, 360 degree values.
+    ##  This is the vector that will store the values.
+    angle <- degrees
     
-  ##  This sets NAs to FALSE
-  fx.na <- function(x) { x[ is.na( x ) ] <- FALSE; x }
-  
-  ##  Given the text azimuths in the dataset, return the quadrant values.
-  ##  This gives a boolean index of the quadrant
-  
-  north <- fx.na(bearings == 'N_NA'| bearings == 'NA_N' | bearings =='N') #|bearings =='N99999'|bearings =='N88888')
-  east <- fx.na(bearings == 'NA_E' | bearings =="E_NA" | bearings =='E') #|bearings =='99999E'|bearings =='88888E')
-  south <- fx.na(bearings == 'S_NA' | bearings =="NA_S"| bearings == 'S') #|bearings =='S99999'|bearings =='S88888')
-  west <- fx.na(bearings == 'NA_W' | bearings =="W_NA" | bearings == 'W') #|bearings =='99999W'|bearings =='88888W')
-  
-  ##north <- fx.na( regexpr('N', bearings) > 0 )
-  ##east  <- fx.na( regexpr('E', bearings) > 0 | bearings == 'EAST')
-  ##south <- fx.na( regexpr('S', bearings) > 0 | bearings == 'SOUTH')
-  ##west <-  fx.na( regexpr('W', bearings) > 0 | bearings == 'WEST')
-
-    ## CJP: this doesn't make any sense - by how north,east,south,west
-    ## defined above, the first condition in these four lines will never be TRUE
-  if(FALSE) {
-      ne <- fx.na( (north & east) | bearings == 'N_E')
-      se <- fx.na( (south & east) | bearings == 'S_E')
-      sw <- fx.na( (south & west) | bearings == 'S_W')
-      nw <- fx.na( (north & west) | bearings == 'N_W') 
-  }
+    ##  This sets NAs to FALSE
+    fx.na <- function(x) { x[ is.na( x ) ] <- FALSE; x }
     
-      ne <- fx.na(bearings == 'N_E')
-      se <- fx.na(bearings == 'S_E')
-      sw <- fx.na(bearings == 'S_W')
-      nw <- fx.na(bearings == 'N_W') 
+    ##  Given the text azimuths in the dataset, return the quadrant values.
+    ##  This gives a boolean index of the quadrant
+    
+    north <- fx.na(bearings == 'N_NA'| bearings == 'NA_N' | bearings =='N') 
+    east <- fx.na(bearings == 'NA_E' | bearings =="E_NA" | bearings =='E') 
+    south <- fx.na(bearings == 'S_NA' | bearings =="NA_S"| bearings == 'S') 
+    west <- fx.na(bearings == 'NA_W' | bearings =="W_NA" | bearings == 'W') 
+    
+    ne <- fx.na(bearings == 'N_E')
+    se <- fx.na(bearings == 'S_E')
+    sw <- fx.na(bearings == 'S_W')
+    nw <- fx.na(bearings == 'N_W') 
+    
+    ## set the unidirectional angles to a degrees 360 
+    angle[ north ] <- 360 # KAH changed this from 0 to 360 to match convention with southwest michigan, where an angle == 0 is a missing/erroreous value
+    angle[ south ] <- 180
+    angle[ east  ] <- 90 
+    angle[ west  ] <- 270
 
-    ##  The cell is in a quadrant, regardless of which.
-  quad <- ne | se | sw | nw
-  
-  ##  Special case of the trees with a unidirectional direction.
-    uni  <- (!quad) & !(north | south | east | west)
-    ## CJP: what does unidirectional mean - based on the code here, aren't these cases where we have
-    ## NA for both bearing and bearingdir in the original data file?
+    ##  convert all angles from a 90-degree value within each quadrat to a 360 degree value.
+    ## 'ne' quadrant is angles of 0-90 starting at north, so doesn't need changing.
+    ## 'se' quadrant is angles of 0-90 starting at south  
+    angle[ se ] <- 180 - degrees[ se ]
+    ## 'sw' quadrant is angles of 0-90 starting at south
+    angle[ sw ] <- 180 + degrees[ sw ]
+    ## 'nw' quadrant is angles of 0-90 starting at north
+    angle[ nw ] <- 360 - degrees [ nw ]
 
-    ## CJP: this doesn't make any sense - uni is only TRUE if none of north/south/east/west are,
-    ## so none of these condition are ever true
-    if(FALSE) {
-        angle[ uni & north ] <- 0
-        angle[ uni & south ] <- 180
-        angle[ uni & east  ] <- 90 
-        angle[ uni & west  ] <- 270
-    }
-    ## CJP: as a result, quad,ne,se,nw,sw do not need to be computed
-  
-   ##  Another set of special cases:
- 
-    angle[ north ] <- degrees[ north ] ## CJP: this has no effect since angle started out equal to degrees
-    ## CJP: would be good to have comment that explains what this is doing.
-  angle[ east ] <- 180 - degrees[ east ]
-  angle[ south ] <- 180 + degrees[ south ]
-  angle[ west ] <- 360 - degrees [ west ]
-  
-  return(angle)
+   return(angle)
   
 }
 
